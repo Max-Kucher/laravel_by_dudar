a5addf8b7e4c5c8c943c98ea92780cb648707bed
AB Max-Kucher <maxkuchirovjob@gmail.com>
2020-05-17 16:25:21 +0300

    Третий урок, создание формы обратной связи

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,4 +4,30 @@
 
 @section('content')
     <h1>Contact page</h1>
+
+    <form action="{{ route('contact-form') }}" method="post">
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Имя</label>
+            <input type="text" id="name" name="name" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input type="text" id="subject" name="subject" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="message">Текст сообщения</label>
+            <textarea id="message" name="message" class="form-control"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-success">Отправить</button>
+    </form>
 @endsection
